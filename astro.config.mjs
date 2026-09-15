ca5b2de868cd14d104bdb5d135f1a8be43f8579e
mbarazandeh110 <m.barazandeh110@gmail.com>
2026-09-15 14:52:02 +0330

    new favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 // import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ekbatan.tech',
   // integrations: [tailwind()],
+  integrations: [sitemap()],
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
